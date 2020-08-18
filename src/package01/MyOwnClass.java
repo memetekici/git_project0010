21556c5bb61463311acf747e61419525e4c2a727
Memet Ekici <meban2009@gmail.com>
2020-08-18 16:05:46 +0200

    the thitd commit from local master

--- src/package01/MyOwnClass.java
+++ src/package01/MyOwnClass.java
@@ -5,6 +5,7 @@ public class MyOwnClass {
 		
 		System.out.println("This is the first changes from local master");
 		System.out.println("This is the second commit from local master");
+		System.out.println("This is the third commit from local master");
 		
 	}
 	
