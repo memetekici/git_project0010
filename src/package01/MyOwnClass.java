fc871c191240abe27543110e16a087fd7c575032
Memet Ekici <meban2009@gmail.com>
2020-08-19 07:19:21 +0200

    the third commit from local Memet

--- src/package01/MyOwnClass.java
+++ src/package01/MyOwnClass.java
@@ -15,6 +15,7 @@ public class MyOwnClass {
 		System.out.println("First commit from Memet branch");
 		
 		System.out.println("The second commit from local Memet");
+		System.out.println("This is the third commit from local Memet");
 		
 	}
 	
